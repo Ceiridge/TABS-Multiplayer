753db44682cb351e864c4ff3d19c60ff3ca76f64
Ceiridge <philipp34533@icloud.com>
2019-04-09 21:37:19 +0200

    Added a booter modifier to hook init and added a basic socketconnection manager

---
+++ TABS-Multiplayer/SocketConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TABS_Multiplayer
+{
+    // The class for handling the socket between the players
+    public class SocketConnection
+    {
+        private static TcpListener tcpServer;
+        private static TcpClient tcpClient;
+
+        private static bool inited = false; // Don't execute the init twice
+
+
+        public static void Init()
+        {
+            if (inited) return;
+            inited = true;
+
+            tcpServer = new TcpListener(IPAddress.Any, 8042); // TODO: Change the port if you want
+            tcpClient = new TcpClient();
+
+            
+        }
+
+        public static TcpClient getTcpClient()
+        {
+            return tcpClient;
+        }
+
+        public static TcpListener getTcpServer()
+        {
+            return tcpServer;
+        }
+    }
+}
---
+++ TABS-Multiplayer/TABSBooterModifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TABS_Multiplayer;
+
+#pragma warning disable CS0626
+class patch_TABSBooter : TABSBooter
+{
+    // Hook the init method
+    public extern void orig_Init();
+    public new void Init()
+    {
+        SocketConnection.Init(); // Init the socket manager
+    }
+}
