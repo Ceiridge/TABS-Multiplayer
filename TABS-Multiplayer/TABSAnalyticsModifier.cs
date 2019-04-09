d4cbcbdfbb8d82dfc57398cfd175c01dd3edc14c
Ceiridge <philipp34533@icloud.com>
2019-04-09 21:13:49 +0200

    Prevent TABS from sending analytics and deleted test hooks

---
+++ TABS-Multiplayer/TABSAnalyticsModifier.cs
@@ -0,0 +1,15 @@
+﻿using Landfall.TABS;
+using Landfall.TABS.Workshop;
+using System;
+
+#pragma warning disable CS0626
+
+// Prevent the client from sending false analytics data
+class patch_TABSAnalytics : TABSAnalytics
+{
+    public static extern void orig_CampaignBattleEnded();
+    public new static void CampaignBattleEnded(bool didWin, TABSCampaignLevelAsset map) { } // Completely override the method
+
+    public static extern void orig_SandboxBattleEnded();
+    public new static void SandboxBattleEnded(bool redWin, MapAsset map) { } // Completely override the method
+}
