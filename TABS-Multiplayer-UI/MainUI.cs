61c085bbbd8e9fe6f88111ecd87c020c1e933891
Ceiridge <philipp34533@icloud.com>
2019-04-10 21:07:58 +0200

    Added form button functionality

--- TABS-Multiplayer-UI/MainUI.cs
+++ TABS-Multiplayer-UI/MainUI.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -15,6 +17,7 @@ namespace TABS_Multiplayer_UI
     {
         public static bool DEBUG = true; // Debug Mode
         public static TcpClient tcp;
+        public static BinaryWriter uiWriter;
 
         ScreenshareForm screenshareForm;
 
@@ -40,12 +43,19 @@ namespace TABS_Multiplayer_UI
 
         private void hostBtn_Click(object sender, EventArgs e) // Host Button
         {
-            
+            WriteToTABS(StrToByte("HOSTNOW")); // Send the host command
+            button1.Enabled = false; // Disable the host btn
         }
 
         private void connectBtn_Click(object sender, EventArgs e) // Connect Button
         {
-
+            try
+            {
+                WriteToTABS(StrToByte("CONNECT|" + IPAddress.Parse(textBox1.Text).ToString())); // Send connect cmd + ip with parse checking
+            } catch(Exception)
+            {
+                MessageBox.Show("Invalid IP!", "Error");
+            }
         }
 
         private void tcpWaiter_Tick(object sender, EventArgs e)
@@ -61,7 +71,36 @@ namespace TABS_Multiplayer_UI
 
         private void TCPReceiver()
         {
+            using (NetworkStream nStream = tcp.GetStream()) // Get the stream
+            {
+                using (BinaryReader reader = new BinaryReader(nStream)) // Read it
+                {
+                    uiWriter = new BinaryWriter(nStream); // Set the writer
 
+                    while (true) // Permanently try to read
+                    {
+                        string newData = reader.ReadString();
+                    }
+                }
+            }
+        }
+
+        private void WriteToTABS(byte[] content)
+        {
+            if (tcp.Connected)
+            {
+                uiWriter.Write(content);
+            }
+        }
+
+        private static byte[] StrToByte(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        private static string ByteToStr(byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(bytes);
         }
     }
 }
