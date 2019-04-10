d40e28d05ec651b82afbcd9d30ef54043ae99453
Ceiridge <philipp34533@icloud.com>
2019-04-10 17:13:59 +0200

    Changed the assembly info

--- TABS-Multiplayer-UI/Properties/AssemblyInfo.cs
+++ TABS-Multiplayer-UI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("TABS-Multiplayer-UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TABS-Multiplayer UI")]
+[assembly: AssemblyDescription("The user interface for the TABS-Multiplayer mod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TABS-Multiplayer-UI")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("TABS-Multiplayer UI")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
