756b9cfb6c1804721448993b8b649db20251dec8
Ceiridge <philipp34533@icloud.com>
2019-04-10 17:01:49 +0200

    Designed form and added waitPanel

--- TABS-Multiplayer-UI/Form1.Designer.cs
+++ TABS-Multiplayer-UI/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.waitPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.waitPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // waitPanel
+            // 
+            this.waitPanel.Controls.Add(this.label1);
+            this.waitPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.waitPanel.Location = new System.Drawing.Point(0, 0);
+            this.waitPanel.Name = "waitPanel";
+            this.waitPanel.Size = new System.Drawing.Size(702, 373);
+            this.waitPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TABS not detected yet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.ClientSize = new System.Drawing.Size(702, 373);
+            this.Controls.Add(this.waitPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "TABS-Multiplayer UI";
+            this.TopMost = true;
+            this.waitPanel.ResumeLayout(false);
+            this.waitPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel waitPanel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
