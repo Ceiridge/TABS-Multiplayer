163b30d65a9c3161745888ba953e050156848b42
Ceiridge <philipp34533@icloud.com>
2019-04-10 19:03:00 +0200

    Forced windowed mode

---
+++ TABS-Multiplayer/GlobalSettingsHandlerModifier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+#pragma warning disable CS0626
+class patch_GlobalSettingsHandler : GlobalSettingsHandler
+{
+    public static extern FullScreenMode orig_GetFullScreenMode(int value);
+    public static new FullScreenMode GetFullScreenMode(int value)
+    {
+        return FullScreenMode.Windowed; // Always return windowed to force the mode
+    }
+}
