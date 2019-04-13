fc3b1e2bc7149f9d0f649b202f35b17f79481465
Ceiridge <philipp34533@icloud.com>
2019-04-13 04:11:53 +0200

    Prevent the client from changing their map with the sidebar

---
+++ TABS-Multiplayer/SandboxGameModeHandlerModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using TABS_Multiplayer;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable CS0626
+namespace Landfall.TABS
+{
+    class patch_SandboxGameModeHandler : SandboxGameModeHandler
+    {
+
+        public extern void orig_OnMapChanged(int newMap);
+        public void OnMapChanged(int newMap)
+        {
+            if (SocketConnection.GetIsServer())
+                orig_OnMapChanged(newMap); // Prevent the client from changing their map with the sidebar
+        }
+    }
+}
